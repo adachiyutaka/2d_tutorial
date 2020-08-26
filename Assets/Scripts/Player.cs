5c6e9697b41c1f40ec829edd5737d2d8fa0f1206
adachiyutaka <adachi.yutaka@gmail.com>
2020-08-26 16:41:23 +0900

    ジャンプ機能の実装

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float gravity;
+    public float jumpSpeed;
+    public float jumpHeight;
+    public float jumpLimitTime;
     public GroundCheck ground;
+    public GroundCheck head;
+    public AnimationCurve dashCurve;
+    public AnimationCurve jumpCurve;
 
     private Animator anim = null;
     private Rigidbody2D rb = null;
     private bool isGround = false;
+    private bool isHead = false;
+    private bool isJump = false;
+    private float jumpPos = 0.0f;
+    private float dashTime,jumpTime;
+    private float beforeKey;  //New
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,29 +32,94 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         isGround = ground.IsGround();
+        isHead = head.IsGround();
 
         float horizontalKey = Input.GetAxis("Horizontal");
+        float verticalKey = Input.GetAxis("Vertical"); 
+
         float xSpeed = 0.0f;
+        float ySpeed = -gravity;
+
+        // 左右移動の設定
         if(horizontalKey > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
             anim.SetBool("run", true);
+            dashTime += Time.deltaTime;
             xSpeed = speed;
         }
         else if (horizontalKey < 0) 
         {
             transform.localScale = new Vector3(-1, 1, 1);
             anim.SetBool("run", true);
+            dashTime += Time.deltaTime;
             xSpeed = -speed;
         }
         else
         {
             anim.SetBool("run", false);
             xSpeed = 0.0f;
+            dashTime = 0.0f;
         }
-        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
+
+        // ジャンプの設定
+        if(isGround)
+        {
+        if (verticalKey > 0)
+            {
+                ySpeed = jumpSpeed;
+                jumpPos = transform.position.y;
+                isJump = true;
+                jumpTime = 0.0f;
+            }
+            else
+            {
+                isJump = false;
+            } 
+        }
+        else if(isJump)
+        {
+            //上方向キーを押しているか
+            bool pushUpKey = verticalKey > 0;
+            //現在の高さが飛べる高さより下か
+            bool canHeight = jumpPos + jumpHeight > transform.position.y;
+            //ジャンプ時間が長くなりすぎてないか
+            bool canTime = jumpLimitTime > jumpTime;
+
+            if (pushUpKey && canHeight && canTime && !isHead)
+            {
+                ySpeed = jumpSpeed;
+                jumpTime += Time.deltaTime;
+            }
+            else
+            {
+                isJump = false;
+                jumpTime = 0.0f; 
+            }
+        }
+
+        // 走る方向が逆になっているかどうかの判定
+        // 方向が反転している場合はdashTimeをリセットする
+        if (horizontalKey > 0 && beforeKey < 0)
+        {
+            dashTime = 0.0f;
+        }
+        else if (horizontalKey < 0 && beforeKey > 0)
+        {
+            dashTime = 0.0f;
+        }
+        beforeKey = horizontalKey;
+
+        // xSpeedとySpeedの値をAnimationCurveと各継続時間から取得
+        xSpeed *= dashCurve.Evaluate(dashTime);
+        if (isJump)
+        {
+            ySpeed *= jumpCurve.Evaluate(jumpTime);
+        }
+        
+        rb.velocity = new Vector2(xSpeed, ySpeed);
     }
 }
