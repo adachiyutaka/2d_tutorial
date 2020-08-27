c1df206899ec98263beb974843f28abaf5763bc8
adachiyutaka <adachi.yutaka@gmail.com>
2020-08-27 16:04:51 +0900

    画面に映っているかどうかの判定を設定

--- Assets/Scripts/Enemy_Zako.cs
+++ Assets/Scripts/Enemy_Zako.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Enemy_Zako : MonoBehaviour
 {
+    private SpriteRenderer sr = null; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (sr.isVisible)
+        {
+            Debug.Log("画面に見えている");
+        } 
     }
 }
