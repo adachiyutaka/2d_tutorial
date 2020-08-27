3c17a676bae15724cc9e5b2230f1a6a9b477ba76
adachiyutaka <adachi.yutaka@gmail.com>
2020-08-27 16:08:49 +0900

    敵の移動を作成
    移動方向を指定するフラグの作成
    移動速度パラメーターの作成
    重力パラメーターの作成
    Rigidbody2Dとvelocityの直接代入による移動処理の作成

--- Assets/Scripts/Enemy_Zako.cs
+++ Assets/Scripts/Enemy_Zako.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Enemy_Zako : MonoBehaviour
 {
+    [Header("移動速度")]public float speed;
+    [Header("重力")]public float gravity;
     [Header("画面外でも行動する")] public bool nonVisibleAct; 
-    private SpriteRenderer sr = null; 
+
+     private Rigidbody2D rb = null;
+     private SpriteRenderer sr = null;
+     private bool rightTleftF = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>(); 
     }
 
@@ -18,7 +24,21 @@ public class Enemy_Zako : MonoBehaviour
     {
         if (sr.isVisible || nonVisibleAct)
         {
-            Debug.Log("画面に見えている");
+            int xVector = -1;
+            if (rightTleftF)
+            {
+                xVector = 1;
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            rb.velocity = new Vector2(xVector * speed, -gravity);
         } 
+        else
+        {
+            rb.Sleep();
+        }
     }
 }
