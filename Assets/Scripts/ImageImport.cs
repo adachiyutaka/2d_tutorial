991b6b5e6e11704cf5b8c3effc4836312f1c75a2
adachiyutaka <adachi.yutaka@gmail.com>
2020-09-04 17:21:21 +0900

    GET通信の確認

---
+++ Assets/Scripts/ImageImport.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ImageImport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(GetText());
+    }
+ 
+    private IEnumerator GetText() {
+        UnityWebRequest request = UnityWebRequest.Get("http://127.0.0.1:3000/games/image");
+ 
+        // リクエスト送信
+        yield return request.SendWebRequest();
+ 
+        // 通信エラーチェック
+        if (request.isError) {
+            Debug.Log(request.error);
+        } else {
+            if (request.responseCode == 200) {
+                // UTF8文字列として取得する
+                string test = request.downloadHandler.text;
+                Debug.Log(test);
+                // バイナリデータとして取得する
+                // byte[] results = request.downloadHandler.data;
+            }
+        }
+    }
+}
